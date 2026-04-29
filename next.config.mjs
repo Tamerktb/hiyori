4c1d6d79fcbc18b7e66e43294e518530b4c814d7
Tamer <tamerktb2003@gmail.com>
2026-04-29 04:05:45 -0700

    force HTTPS with HSTS header

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
